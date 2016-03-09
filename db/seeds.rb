80fbba25a38ee285faaa4a1494f4f2e9a09f79dd
Pablo Marrero <pablo.marrero@gmail.com>
2016-03-09 12:01:55 -0300

    Add creation of users and profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+350.times do |i|
+  user = User.create!(
+    email:  Faker::Internet.user_name + i.to_s +
+            "@#{Faker::Internet.domain_name}",
+    password: 'cambiameya',
+    password_confirmation: 'cambiameya'
+  )
+  Profile.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    user_id: user.id
+  )
+end
